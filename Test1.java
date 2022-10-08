1ed14b645cabb0de58868a93ed78325a2629b3fc
AshishParteNimap <114918987+AshishParteNimap@users.noreply.github.com>
2022-10-08 11:02:21 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -5,27 +5,29 @@ public class Test1 {
 	//primitive datatype
 			int a=1;
 			long l=100;
+	//reference datatype
+			String name="hello";
 	public static void main(String[] args) {
 		int b=18;
 		long d=2089;
-		String name="hello";
+		
 		String newName="nameOfInstitute";
 		System.out.println("primitive datatypes :"+b+" "+d);
 		
 		
 		Test1 primitive=new Test1();
 		System.out.println("primitive datatype :"+primitive.a);
-		System.out.println("primitive datatype :"+primitive.d);
+		System.out.println("primitive datatype :"+primitive.l);
 
-		//refrence datatype
+		//reference datatype
 		Test1 obj1=new Test1();
 		
 		Test1 refrenceObj=obj1;
 		refrenceObj.name="Nimap Infoetch";
 		refrenceObj.a=23;
 		
-		System.out.println("refrence datatype :"+refrenceObj.a);
-		System.out.println("refrence datatype :"+refrenceObj.name);
+		System.out.println("reference datatype :"+refrenceObj.a);
+		System.out.println("reference datatype :"+refrenceObj.name);
 		
 		
 	}
